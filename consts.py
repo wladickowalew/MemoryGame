26d9cb7ded406b63d295c0b4753b7db778e34d3c
kowlad <wladickowalew@gmail.com>
2023-04-13 11:54:44 +0300

    final commit

---
+++ consts.py
@@ -0,0 +1,14 @@
+# strings
+ALPHA = "QWERTYUIOPASDFGHJKLZXCVBNM"
+WINDOW_TITLE = "Игра на память"
+DEFAULT_CELL_VALUE = " "
+END_WINDOW_TITLE = "Конец игры"
+END_WINDOW_MESSAGE = "Поздровляем, вы победили. Хотите сыграть ещё?"
+
+# Numbers
+CELL_WIDTH, CELL_HEIGHT, PADDING = 50, 50, 15  # field cell size
+FIELD_ROW, FIELD_COLUMN = 3, 4  # field size
+TIMER_DELAY = 2
+
+# Other
+CELL_FONT = ("Arial Bold", 35)
--- main.py
+++ main.py
@@ -2,14 +2,15 @@ import random
 from tkinter import *
 from tkinter.messagebox import askyesno
 from threading import Timer
+from consts import *
 
 
-def func(event):
+def button_click(event):
     global current, blocked, pairs
     btn = event.widget
     if blocked:
         return
-    if btn["text"] != " ":
+    if btn["text"] != DEFAULT_CELL_VALUE:
         return
     if current:
         btn.configure(text=btn.v)
@@ -20,7 +21,7 @@ def func(event):
                 end_game()
         else:
             blocked = True
-            t = Timer(2, lambda: hide_buttons(current, btn))
+            t = Timer(TIMER_DELAY, lambda: hide_buttons(current, btn))
             t.start()
     else:
         btn.configure(text=btn.v)
@@ -28,8 +29,7 @@ def func(event):
 
 
 def end_game():
-    gamer_over = askyesno(title="Конец игры",
-                          message="Поздровляем, вы победили. Хотите сыграть ещё?")
+    gamer_over = askyesno(title=END_WINDOW_TITLE, message=END_WINDOW_MESSAGE)
     if gamer_over:
         clear_field()
     else:
@@ -38,58 +38,50 @@ def end_game():
 
 def clear_field():
     global current, blocked, pairs, v
-    current, blocked, pairs = None, False, n * m // 2
-    v = create_field(n, m)
-    for mas in bs:
-        for button in mas:
-            button.configure(text=" ")
-    for i in range(n):
-        for j in range(m):
+    current, blocked, pairs = None, False, FIELD_ROW * FIELD_COLUMN // 2
+    v = create_field(FIELD_ROW, FIELD_COLUMN)
+    for i in range(FIELD_ROW):
+        for j in range(FIELD_COLUMN):
             bs[i][j].v = v[i][j]
+            bs[i][j].configure(text=DEFAULT_CELL_VALUE)
 
 
 def hide_buttons(btn1, btn2):
     global current, blocked
-    btn1.configure(text=" ")
-    btn2.configure(text=" ")
+    btn1.configure(text=DEFAULT_CELL_VALUE)
+    btn2.configure(text=DEFAULT_CELL_VALUE)
     current = None
     blocked = False
 
 
-def create_field(n, m):
-    ALPHA = "QWERTYUIOPASDFGHJKLZXCVBNM"
-    pairs = random.sample(ALPHA, n * m // 2)
+def create_field():
+    pairs = random.sample(ALPHA, FIELD_ROW * FIELD_COLUMN // 2)
     pairs += pairs
     random.shuffle(pairs)
     v = []
-    for i in range(n):
-        mas = [pairs[i * m + j] for j in range(m)]
+    for i in range(FIELD_ROW):
+        mas = [pairs[i * FIELD_COLUMN + j] for j in range(FIELD_COLUMN)]
         v.append(mas)
     return v
 
 
 window = Tk()
-window.title("Память")
-w, h, d = 50, 50, 15
-n, m = 3, 4  # чтобы игра работала, хотя бы одно число должно быть чётным
-pairs = n * m // 2
-fw = w * m + (m + 1) * d
-fh = h * n + (n + 1) * d
+window.title(WINDOW_TITLE)
+fw = CELL_WIDTH * FIELD_COLUMN + (FIELD_COLUMN + 1) * PADDING  # ширина окна
+fh = CELL_HEIGHT * FIELD_ROW + (FIELD_ROW + 1) * PADDING  # высота окна
 window.geometry(f"{fw}x{fh}")
-current = None
-blocked = False
-bs = []
-step = 0
-v = create_field(n, m)
-for i in range(n):
+
+pairs = FIELD_ROW * FIELD_COLUMN // 2
+current, blocked = None, False
+bs, v = [], create_field()
+for i in range(FIELD_ROW):
     row = []
-    for j in range(m):
-        btn = Button(window, text=" ",
-                     font=("Arial Bold", 35))
-        btn.place(x=d * (j + 1) + w * j,
-                  y=d * (i + 1) + h * i,
-                  width=w, height=h)
-        btn.bind("<Button-1>", func)
+    for j in range(FIELD_COLUMN):
+        btn = Button(window, text=DEFAULT_CELL_VALUE, font=CELL_FONT)
+        btn.place(x=PADDING * (j + 1) + CELL_WIDTH * j,
+                  y=PADDING * (i + 1) + CELL_HEIGHT * i,
+                  width=CELL_WIDTH, height=CELL_HEIGHT)
+        btn.bind("<Button-1>", button_click)
         btn.v = v[i][j]
         row.append(btn)
     bs.append(row)
